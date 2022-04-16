c5243d4c6f1d00cc821230734bf8b5dfc9512e16
Muthukumaran-hutech <muthu@hutechsolutions.com>
2022-04-16 16:22:57 +0530

    Activities added

--- app/src/main/java/com/example/topaz/Activities/IntroSlider.kt
+++ app/src/main/java/com/example/topaz/Activities/IntroSlider.kt
@@ -3,156 +3,115 @@ package com.example.topaz.Activities
 import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.SeekBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.example.topaz.Adapters.IntroSliderAdapter
+import com.example.topaz.Activities.IndicatorLayout
 import com.example.topaz.Fragments.SliderFragment
 import com.example.topaz.Fragments.SliderFragment2
 import com.example.topaz.Fragments.SliderFragment3
 import com.example.topaz.R
 import java.util.ArrayList
 
+
 class IntroSlider : AppCompatActivity() {
     private var skipBtn: TextView? = null
     lateinit var nextBtn: ImageButton
-    lateinit var view_pager: ViewPager
+    lateinit var viewpager: ViewPager2
+    //private var indicatorLayout: SeekBar? = null
     private var getStarted: Button? = null
-    var indicator1: TextView? = null
-    var indicator2: TextView? = null
-    var indicator3: TextView? = null
-    val fragment = SliderFragment()
-    val fragment2 = SliderFragment2()
-    val fragment3 = SliderFragment3()
-    lateinit var adapter: myPagerAdapter
+    private lateinit var indicatorLayout: IndicatorLayout
+    //lateinit var adapter1: MyPagerAdapter
     lateinit var activity: Activity
+
+    // lateinit var indicatorLayout: SeekBar
+    private val fragmentList = ArrayList<Fragment>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // making the status bar transparent
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ) {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
         setContentView(R.layout.activity_intro_slider)
+
 
 
 
         skipBtn = findViewById(R.id.btn_skip)
         nextBtn = findViewById(R.id.btn_next)
-        view_pager =findViewById(R.id.view_pager)
-
-
-
-
-        adapter = myPagerAdapter(supportFragmentManager)
-        adapter.list.add(fragment)
-        adapter.list.add(fragment2)
-        adapter.list.add(fragment3)
+         indicatorLayout = findViewById(R.id.indicatorLayout)
+        viewpager = findViewById(R.id.view_pager)
         getStarted = findViewById(R.id.get_start)
         activity = this
 
-        view_pager?.adapter = adapter
-        skipBtn?.setOnClickListener{
-            startActivity(Intent(activity,LoginActivity::class.java))
-            finish()
-        }
-        /* getStarted?.setOnClickListener{
-             startActivity(Intent(activity,LoginActivity::class.java))
-             finish()
-         }*/
-        nextBtn.setOnClickListener {
-            view_pager.currentItem++
-        }
-        view_pager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-            override fun onPageScrolled(
-                position: Int,
-                positionOffset: Float,
-                positionOffsetPixels: Int
-            ) {
+        val adapter = IntroSliderAdapter(this)
+        viewpager.adapter = adapter
+        fragmentList.addAll(
+            listOf(
+                SliderFragment(), SliderFragment2(), SliderFragment3()
 
-            }
+            )
+        )
+
+        adapter.setFragmentList(fragmentList)
+          indicatorLayout.setIndicatorCount(adapter.itemCount)
+          indicatorLayout.selectCurrentPosition(0)
+          registerListeners()
+    }
+
+    private fun registerListeners() {
+        viewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
 
 
             override fun onPageSelected(position: Int) {
-                if (position == adapter.list.size - 1) {
-                    getStarted?.setOnClickListener {
-                        startActivity(Intent(activity, EmailLogin::class.java))
-                        finish()
+                //   indicatorLayout.selectCurrentPosition(position)
+                if (position < fragmentList.lastIndex) {
+                    skipBtn?.visibility = View.VISIBLE
+                    nextBtn.visibility = View.VISIBLE
+                    getStarted?.visibility = View.GONE
 
-                    }
                 } else {
-                    nextBtn.setOnClickListener {
-                        view_pager.currentItem++
-                    }
-                }
-                when (view_pager.currentItem) {
-                    0 -> {
-                       indicator1?.setTextColor(Color.parseColor("#00ADEF"))
-                        indicator2?.setTextColor(Color.parseColor("#FFFFFFFF"))
-                        indicator3?.setTextColor(Color.parseColor("#FFFFFFFF"))
-                        indicator1?.visibility = View.VISIBLE
-                        indicator2?.visibility = View.VISIBLE
-                        indicator3?.visibility = View.VISIBLE
-                        getStarted?.visibility = View.GONE
-                        skipBtn?.visibility = View.VISIBLE
-                        nextBtn.visibility = View.VISIBLE
-                    }
-                    1 -> {
-                        indicator1?.setTextColor(Color.parseColor("#FFFFFFFF"))
-                        indicator2?.setTextColor(Color.parseColor("#00ADEF"))
-                        indicator3?.setTextColor(Color.parseColor("#FFFFFFFF"))
-                        indicator1?.visibility = View.VISIBLE
-                        indicator2?.visibility = View.VISIBLE
-                        indicator3?.visibility = View.VISIBLE
-                        getStarted?.visibility = View.GONE
-                        skipBtn?.visibility = View.VISIBLE
-                        nextBtn.visibility = View.VISIBLE
-
-                    }
-                    2 -> {
-                        indicator1?.visibility = View.GONE
-                        indicator2?.visibility = View.GONE
-                        indicator3?.visibility = View.GONE
-                        getStarted?.visibility = View.VISIBLE
-                        skipBtn?.visibility = View.GONE
-                        nextBtn.visibility = View.GONE
-                    }
+                    skipBtn?.visibility = View.GONE
+                    nextBtn.visibility = View.GONE
+                    getStarted?.visibility = View.VISIBLE
                 }
             }
-
-
-            override fun onPageScrollStateChanged(state: Int) {
-
-            }
-
         })
-
-    /*    val timer = object: CountDownTimer(20000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {}
-
-            override fun onFinish() {}
+        skipBtn?.setOnClickListener {
+            startActivity(Intent(this, EmailLogin::class.java))
+            finish()
         }
-        timer.start()*/
+
+        nextBtn.setOnClickListener {
+            viewpager.currentItem++
+        }
+
+        getStarted?.setOnClickListener {
+            val position = viewpager.currentItem
+            if (position < fragmentList.lastIndex) {
+                viewpager.currentItem = position + 1
+            } else {
+                startActivity(Intent(this, EmailLogin::class.java))
+                finish()
+            }
+        }
+
+
     }
 
-
-
-    class myPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
-
-        val list: MutableList<Fragment> = ArrayList()
-
-
-        override fun getCount(): Int {
-            return list.size
-        }
-
-        override fun getItem(position: Int): Fragment {
-            return list[position]
-        }
-
-    }
 }
--- app/src/main/java/com/example/topaz/Activities/LoginActivity.kt
+++ app/src/main/java/com/example/topaz/Activities/LoginActivity.kt
@@ -7,9 +7,17 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import com.example.topaz.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthProvider
 import com.hbb20.CountryCodePicker
 
 class LoginActivity : AppCompatActivity() {
+
+    private var forceResendingToken: PhoneAuthProvider.ForceResendingToken? = null
+
+    private var mcallBacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks? = null
+    private var mVerificationId: String? = null
+    private lateinit var firebaseAuth: FirebaseAuth
 
     private var continueBtn: Button? = null
     private var countryCode: CountryCodePicker? = null
@@ -29,6 +37,38 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(activity,OtpVerfification::class.java))
             finish()
         }
+
+/*
+        // show password rules in text view
+        textView.text = "Password minimum length 8"
+        textView.append("\n1 uppercase")
+        textView.append("\n1 number")
+        textView.append("\n1 special character")
+
+
+        // add text changed listener for edit text
+        editText.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?,
+                                           start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?,
+                                       start: Int, before: Int, count: Int) {
+                s?.apply {
+                    // check user input a valid formatted password
+                    if (isValidPassword() && toString().length>=8) {
+                        editText.error == null
+                    }else{
+                        // show error on input invalid password
+                        editText.error = "invalid password."
+                    }
+                }
+            }
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })*/
 
     }
 }
